250612231e2535b39cf89919f3633d2eba9a648d
mustapha chatbi <chatbimdev@gmail.com>
2020-06-07 17:37:08 +0100

    connect to mongodbcloud with mongoose

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const router = require("./routes/index");
 const port = 3000;
 
 app.use("/", router);
 
+if(process.env.NODE_ENV !== "production"){
+  require('dotenv').config();
+}
+
+const uri = process.env.DATABASE_URL;
+
+mongoose.connect(uri , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000
+}).then(res => console.log(res))
+.catch(err => console.log(err));
+
+
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`${process.env.DATABASE_URL}`)
 );
